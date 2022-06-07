c3bef3e7d0f8523ae6c95b893c5f6efb50f97d6c
diego <diego.cortinagarcia@gmail.com>
2022-06-07 09:05:17 +0200

    added bootstrap and routes for add and list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { EditComponent } from './Persona/edit/edit.component';
+import { AddComponent } from './Persona/add/add.component';
+import { ListarComponent } from './Persona/listar/listar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'listar', component: ListarComponent },
+  { path: 'add', component: AddComponent },
+  { path: 'edit', component: EditComponent },
+  { path: '**', redirectTo: 'listar' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
